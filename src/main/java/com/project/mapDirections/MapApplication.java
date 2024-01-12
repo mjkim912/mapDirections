0261c31c087b24795e0649cff2aecca69c06e7d0
mjkim912 <sinceree912@gmail.com>
2024-01-12 10:12:21 +0900

    필요없는 파일들 삭제

--- src/main/java/com/project/mapDirections/MapApplication.java
+++ src/main/java/com/project/mapDirections/MapApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SearchBlogsApplication {
+public class MapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SearchBlogsApplication.class, args);
+		SpringApplication.run(MapApplication.class, args);
 	}
 
 }
